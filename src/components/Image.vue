127b73f06ec85052645630853aed5435e96dbc0b
Jack <ajackofsometrades@gmail.com>
2018-08-24 16:29:49 -0700

    the basic component for the image views

---
+++ src/components/Image.vue
@@ -0,0 +1,28 @@
+<template>
+  <li @click="handleClick">
+    <img :src="album.image[index]">
+  </li>
+</template>
+<script>
+
+export default {
+  props: {
+    album: Object,
+    selectedAlbum: Object,
+    onSelect: Function,
+  },
+  methods: {
+    handleClick() {
+      this.onSelect(this.album);
+    }
+  },
+};
+</script>
+
+<style scoped>
+
+p {
+  margin: 5%;
+}
+
+</style>
