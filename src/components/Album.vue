348f51a1b15fc3ec683f05c61cd7d563ceec5f6d
Jack <ajackofsometrades@gmail.com>
2018-08-23 17:37:49 -0700

    connecting albums and album components

---
+++ src/components/Album.vue
@@ -0,0 +1,32 @@
+<template>
+  <li>
+    <!-- <router-link :to="`/albumDB/${album.key}`"> -->
+      <h3>{{ album.title }}</h3>
+      <p>{{ album.description }}</p>
+    <!-- </router-link> -->
+  </li>
+</template>
+
+<script>
+export default {
+  props: {
+    album: Object,
+    // selected: Object,
+    // onSelect: Function
+  },
+  // methods: {
+  //   handleClick() {
+  //     this.onSelect(this.album);
+  //   }
+  // }
+};
+</script>
+
+<style scoped>
+
+a {
+  text-decoration: none;
+  color: initial;
+}
+
+</style>
--- src/components/Albums.vue
+++ src/components/Albums.vue
@@ -1,22 +1,49 @@
 <template>
-  <main>
-    <h1 v-for="album in albumDB"
+  <ul>
+    <Album v-for="album in albumDB"
       :key="album.title"
       :album="album"  
-    >
-      {{album.title}}
-    </h1>
-  </main>
+    />
+    <li id="add-album"></li>
+  </ul>
 </template>
 
 <script>
+import Album from './Album';
+
 export default {
   props: {
     albumDB: Array
+  },
+  components: {
+    Album
   }
 };
 </script>
 
 <style scoped>
+
+ul {
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
+  grid-gap: 20px;
+
+}
+
+li {
+  width: 250px;
+  height: 250px;
+  justify-self: center;
+  list-style: none;
+  text-align: center;
+  border: 1px solid #aaa;
+}
+
+#add-album {
+  background-image: url('../assets/plus-sign.png');
+  background-size: contain;
+  background-repeat: no-repeat;
+
+}
 
 </style>
