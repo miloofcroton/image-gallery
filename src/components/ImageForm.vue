60814ca4f0fa0489865d0c97abdaee49ee756258
Jack <ajackofsometrades@gmail.com>
2018-08-24 16:30:20 -0700

    start of image form for newimage page

---
+++ src/components/ImageForm.vue
@@ -0,0 +1,53 @@
+<template>
+  <main>
+    <h1>Add a new image</h1>
+    <form @submit.prevent="handleSubmit">
+      <label>
+        Url: <input v-model="imageURL">
+      </label>
+      <button type="submit">Add</button>
+      <button v-if="onCancel" type="button" @click="onCancel">Cancel</button>
+    </form>
+  </main> 
+
+</template>
+
+
+
+<script>
+export default {
+  props: {
+    album: Object,
+    onComplete: Function,
+    onCancel: Function
+  },
+  data() {
+    return {
+      key: '',
+      imageURL: '',
+    };
+  },
+  created() {
+    const image = this.image;
+    this.imageURL = image.title;
+  },
+  methods: {
+    handleSubmit() {
+      const image = {
+        title: this.imageURL,
+      };
+
+      // the rest of this is wrong
+
+      // album.key = this.album.key;
+      // this.onComplete(album);
+    }
+  }
+};
+
+</script>
+
+<style>
+
+</style>
+
