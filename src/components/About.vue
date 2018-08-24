ff19ab5092b8f95d145296228043978fde5333a3
Jack <ajackofsometrades@gmail.com>
2018-08-24 16:25:42 -0700

    lorem ipsum

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,6 +1,15 @@
 <template>
   <main>
-    <h1>How my life got turned flipped upside down</h1>
+    <h2>How my life got turned flipped upside down</h2>
+    <section>
+      <p>
+        Lorem ipsum dolor amet keytar vexillologist pitchfork cray, kickstarter dreamcatcher kitsch copper mug food truck microdosing selvage poke locavore leggings. Chicharrones vinyl cray semiotics chillwave blog affogato seitan palo santo meditation artisan pabst. Vexillologist deep v tilde gentrify yuccie. +1 succulents biodiesel palo santo leggings actually four dollar toast letterpress austin fanny pack affogato. Cliche hella mlkshk hot chicken seitan heirloom shaman twee yr gastropub jianbing.
+      </p>
+
+      <p>
+        8-bit pok pok hexagon, flannel blog readymade PBR&B venmo la croix knausgaard fixie synth. Squid art party hexagon kombucha shabby chic, mlkshk retro. Kombucha keytar art party retro actually activated charcoal, post-ironic ethical. Small batch paleo pabst fam. Taxidermy franzen food truck swag bitters. Chia brooklyn bicycle rights ugh tacos jean shorts knausgaard kickstarter raclette humblebrag franzen bespoke biodiesel 3 wolf moon hammock. +1 keffiyeh biodiesel master cleanse banh mi iceland.
+      </p>
+    </section>
   </main>
   
 </template>
@@ -12,5 +21,13 @@ export default {
 </script>
 
 <style scoped>
+
+h2 {
+  font-size: 32px;
+}
+
+p {
+  margin: 30px;
+}
 
 </style>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,6 +1,15 @@
 <template>
   <main>
-    <h1>Home Page</h1>
+    <h2>Home Page</h2>
+    <section>
+      <p>
+        Letterpress paleo drinking vinegar knausgaard freegan. Wolf brooklyn iPhone banh mi small batch gochujang banjo artisan hexagon photo booth copper mug skateboard put a bird on it chia bicycle rights. Skateboard 90's fam paleo 8-bit occupy health goth plaid heirloom cold-pressed aesthetic flannel. Portland yr adaptogen synth, art party schlitz shabby chic butcher chillwave umami hella ethical sartorial. Ennui gochujang polaroid, keffiyeh tumblr disrupt glossier umami wayfarers schlitz. Tilde pork belly live-edge master cleanse literally marfa ethical mumblecore.
+      </p>
+
+      <p>
+        Keffiyeh vaporware jianbing, neutra stumptown listicle keytar kale chips tote bag brunch tumblr meggings paleo ramps. Taiyaki coloring book williamsburg, helvetica ugh 8-bit street art sriracha. Pok pok flannel man braid kitsch slow-carb fanny pack. Shoreditch kickstarter slow-carb, kombucha deep v hell of photo booth. Cloud bread portland poke quinoa. 90's everyday carry tote bag trust fund etsy. Sustainable vice food truck, pour-over mlkshk bespoke enamel pin blue bottle migas edison bulb bitters prism raw denim umami.
+      </p>
+    </section>
   </main>
 </template>
 
@@ -12,12 +21,16 @@ export default {
 
 <style scoped>
 
-h1 {
+h2 {
   font-size: 32px;
 }
 
 img {
   width: 200px;
+}
+
+p {
+  margin: 30px;
 }
 
 </style>
