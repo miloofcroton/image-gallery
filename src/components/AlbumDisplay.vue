0736fdbd71322320a9bf1a50fe73cf639ecbffcd
Jack <ajackofsometrades@gmail.com>
2018-08-24 16:31:08 -0700

    minor piece to improve album dashboard

---
+++ src/components/AlbumDisplay.vue
@@ -0,0 +1,20 @@
+<template>
+  <section>
+    <h3>{{ album.title }}</h3>
+    <p>{{ album.description }}</p>
+  </section>
+</template>
+
+<script>
+
+export default {
+  props: {
+    album: Object
+  }
+};
+
+</script>
+
+<style>
+
+</style>
