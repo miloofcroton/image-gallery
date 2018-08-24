add29c606154ba75a0f50e457ef099ab66ff336e
Jack <ajackofsometrades@gmail.com>
2018-08-24 16:28:41 -0700

    adding links and a router view in album detail to house the view options

--- src/components/AlbumDetail.vue
+++ src/components/AlbumDetail.vue
@@ -1,11 +1,26 @@
 <template>
   <main>
-    <p> {{ album.name }}</p>
+    <h2> {{ album.title }}</h2>
     <p> {{ album.description }}</p>
+    
+    <p>
+      <router-link to="/detail/:key/new">Add a new image</router-link>
+    </p>
+
+    <p>
+      <router-link to="/detail/:key/thumbnail">Thumbnail View</router-link>
+      <router-link to="/detail/:key/list">List View</router-link>
+      <router-link to="/detail/:key/gallery">Gallery View</router-link>
+    </p>
+
+    <router-view :images="images"></router-view>
+
   </main>
 </template>
 
 <script>
+import albumApi from '../services/albumApi';
+
 
 export default {
   name: 'app',
@@ -13,16 +28,23 @@ export default {
   },
   data() {
     return {
-      album: albums.selected,
-    }
+      album: albumApi.getAlbum(this.$route.params.key),
+    };
   },
   props: {
-    albums: Object,
-    selected: String,
+    selectedAlbum: String,
   }
 };
 </script>
 
 <style scoped>
+
+h2 {
+  font-size: 32px;
+}
+
+p {
+  font-size: 16;
+}
 
 </style>
