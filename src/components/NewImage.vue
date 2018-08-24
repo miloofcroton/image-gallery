4513b57779454952b4f87e4885629e5325b6b459
Jack <ajackofsometrades@gmail.com>
2018-08-24 16:27:32 -0700

    start of newimage page

--- src/components/NewImage.vue
+++ src/components/NewImage.vue
@@ -1,13 +1,29 @@
 <template>
-  
+  <ImageForm 
+    :onComplete="handleAdd"
+    :onCancel="handleEndEdit"
+    :onUpdate="handleUpdate"
+    :image="image"
+  />
 </template>
 
 <script>
-export default {
+import ImageForm from './ImageForm.vue';
+import albumApi from '../services/albumApi';
 
+export default {
+  components: {
+    ImageForm
+  },
+  methods: {
+    handleAdd(album) {
+      const added = albumApi.addImage(album, key); //where to get key?
+      this.$router.push(`/albums/${added.key}`); //this isn't right
+    }
+  }
 };
 </script>
 
-<style scoped>
+<style>
 
 </style>
