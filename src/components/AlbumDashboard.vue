909b1912c36b67624d5236f32e5e4331f492c79c
Jack <ajackofsometrades@gmail.com>
2018-08-24 16:30:46 -0700

    the abstraction for albums and add album components

---
+++ src/components/AlbumDashboard.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="dashboard">
+    <Albums 
+      :albums="albums" 
+      :selected="selectedAlbum"
+      :onSelect="handleSelect"
+    />
+    <AlbumViewer 
+      :album="selectedAlbum"
+      :onUpdate="handleUpdate"
+    />
+  </div>
+</template>
+
+<script>
+import albumApi from '../services/albumApi';
+import Albums from './Albums';
+import AlbumViewer from './AlbumViewer';
+
+export default {
+  name: 'albumDashboard',
+  data() {
+    return {
+      albums: albumApi.getAlbums(),
+      selectedAlbum: null
+    };
+  },
+  components: {
+    Albums,
+    AlbumViewer
+  },
+  methods: {
+    handleSelect(album) {
+      this.selectedAlbum = album;
+    },
+    handleUpdate(updated) {
+      const index = this.albums.findIndex(album => {
+        return album.key === updated.key;
+      });
+
+      if(index !== -1) {
+        this.albums.splice(index, 1, updated);
+        this.selectedAlbum = updated;
+      }
+    }
+  }
+};
+</script>
+
+<style>
+
+
+</style>
