7e822af30d2ec481860a21a7abf90532b82f3c67
Jack <ajackofsometrades@gmail.com>
2018-08-23 17:37:01 -0700

    additional album idea

--- src/services/albumDB.js
+++ src/services/albumDB.js
@@ -1,24 +1,29 @@
 export default [
-    {
-        title: "Football",
-        description: "Try and tackle this!",
-        images: [],
-    },
-    {
-        title: "Monster Trucks",
-        description: "My truck crushes",
-        images: [],
-    },
-    {
-        title: "Predatory Cats",
-        description: "Kill! Kill! Kill!",
-        images: [],
-    },
-    {
-        title: "Energy Drinks",
-        description: "Push it to the liiiiiiiimit",
-        images: [],
-    },            
+  {
+    title: 'Football',
+    description: 'Try and tackle this!',
+    images: [],
+  },
+  {
+    title: 'Monster Trucks',
+    description: 'My truck crushes',
+    images: [],
+  },
+  {
+    title: 'Great Generals',
+    description: 'Far better it is to dare mighty things...',
+    images: [],
+  },
+  {
+    title: 'Predatory Cats',
+    description: 'Kill! Kill! Kill!',
+    images: [],
+  },
+  {
+    title: 'Energy Drinks',
+    description: 'Push it to the liiiiiiiimit',
+    images: [],
+  },
 ];
 
 
