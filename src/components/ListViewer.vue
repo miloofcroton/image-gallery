74dfc3cdde31c1acdc7f00cd8b6b1f251a5a878b
Jack <ajackofsometrades@gmail.com>
2018-08-24 16:26:02 -0700

    start of list and thumbnail viewer

--- src/components/ListViewer.vue
+++ src/components/ListViewer.vue
@@ -1,13 +1,37 @@
 <template>
-  
+  <ul>
+    <h2>I'm the list viewer!</h2>
+    <Image v-for="image in images"
+    :key="image.index"
+    :image="image"
+    />
+  </ul>
 </template>
 
 <script>
-export default {
+import Image from './Image';
 
+export default {
+  props: {
+    images: Array
+  },
+  components: {
+    Image
+  }
 };
 </script>
 
 <style scoped>
+
+
+ul.list-view {
+  display: block;
+  text-align: left;
+}
+
+ul.list-view li {
+  text-align: left;
+  border: none;
+}
 
 </style>
--- src/components/ThumbnailViewer.vue
+++ src/components/ThumbnailViewer.vue
@@ -1,13 +1,32 @@
 <template>
-  
+  <ul class="tile-view">
+    <h2>I'm the thumbnail viewer!</h2>
+    <Image v-for="image in images"
+    :key="image.index"
+    :image="image"
+    />
+  </ul>
 </template>
 
 <script>
-export default {
+import Image from './Image';
 
+export default {
+  props: {
+    images: Array
+  },
+  components: {
+    Image
+  }
 };
 </script>
 
 <style scoped>
+
+ul.tile-view {
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+  grid-gap: 5px;
+}
 
 </style>
